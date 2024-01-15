5dc65244a4bc53b2f467c482b68df2348e292570
Lloyd Woolacott <123055990+lwooly@users.noreply.github.com>
2024-01-15 17:00:41 +0000

    avatar

--- src/Components/EventCard.jsx
+++ src/Components/EventCard.jsx
@@ -8,19 +8,22 @@ import Typography from "@mui/material/Typography";
 import Age from '../Components/Age';
 import Grid from '@mui/material/Grid';
 import BackgroundLetterAvatars from "./Avatar";
+import Box from "@mui/material/Box";
 
 const EventCard = ({ birthday }) => {
     console.log(birthday)
     return (
       <ListItem sx={{display:'flex'}}>
-        <BackgroundLetterAvatars birthday={birthday} />
         <Card sx={{width:'50%', margin:'auto', borderRadius:4}} m={4}>
           <CardContent>
             <Grid container spacing={2}>
               <Grid item xs={12}>
+              <Box sx={{display:'flex', justifyContent:'space-between'}}>
+              <BackgroundLetterAvatars birthday={birthday} />
                 <Typography gutterBottom variant="h4" component="div">
                   {birthday.name}
                 </Typography>
+                </Box>
               </Grid>
               <Grid item xs={6}>
                 <Typography variant="body2" color="text.primary">
