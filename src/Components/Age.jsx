59f584cafcde055e690f3e21c896d9c67bd2fa62
Roxana Mihaila <emmyta93@gmail.com>
2024-01-15 15:00:32 +0000

    fixed age function

--- src/Components/Age.jsx
+++ src/Components/Age.jsx
@@ -5,17 +5,8 @@ function calculateAge({targetDate}) {
 
     //calculate age (years) current year to birth year
     const age = currentDate.getFullYear() - birthDate.getFullYear();
-
-    //calculate age (months) difference from current month to birth month
-    const monthDiff = currentDate.getMonth() - birthDate.getMonth();
-
-
-
-    // if statement to check if the birth month is later in the year than current month,
-    //  "should" subtract 1 from the age if the birthday month hasnt occured during current year
-    if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate()))
   
-    return age - 1;
+    return age;
   }
 
   export default calculateAge;
